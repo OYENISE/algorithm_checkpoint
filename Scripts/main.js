03522afbd643b7ad334c3548f2bb7ce8df321bd2
OYENISE <laferranovo@gmail.com>
2023-05-30 18:28:42 -0700

    script add

---
+++ Scripts/main.js
@@ -0,0 +1,11 @@
+ sum_of_distinct_elements(set1, set2):
+    distinct_elements = [];
+     distinct_elements.append(element)
+     distinct_elements.append(element)
+    return sum(distinct_elements)
+
+set1 = [3, 1, 7, 9]
+set2 = [2, 4, 1, 9, 3]
+result = sum_of_distinct_elements(set1, set2)
+
+print(result)  
