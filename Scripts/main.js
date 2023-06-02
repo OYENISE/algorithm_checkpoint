994c50d3d8344ccef9fb2ecb36e15855bc9f113f
OYENISE <laferranovo@gmail.com>
2023-06-02 16:17:07 -0700

    script add

--- Scripts/main.js
+++ Scripts/main.js
@@ -9,3 +9,21 @@ set2 = [2, 4, 1, 9, 3]
 result = sum_of_distinct_elements(set1, set2)
 
 print(result)  
+
+
+//THIS IS THE CHECK POINT FOR RECUSSION
+function isPalindrome(word) {
+    // Remove any non-alphanumeric characters and convert to lowercase
+    word = word.replace(/[^a-z0-9]/gi, '').toLowerCase();
+  
+    // Check if the word is equal to its reverse
+    return word === word.split('').reverse().join('');
+  }
+  
+  // Example usage
+  console.log(isPalindrome('gag')); // true
+  console.log(isPalindrome('kayak')); // true
+  console.log(isPalindrome('php')); // true
+  console.log(isPalindrome('radar')); // true
+  console.log(isPalindrome('hello')); // false
+  
